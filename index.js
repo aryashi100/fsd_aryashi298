e07f6742d9cce015715bd2c7d72b365e8acaf952
aryashi100 <aryashikhare100@gamil.com>
2025-09-06 21:33:47 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+ const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+        const mobileMenu = document.getElementById('mobileMenu');
+
+        mobileMenuBtn.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
+
+        import express from "express";
+import bodyParser from "body-parser";
+
